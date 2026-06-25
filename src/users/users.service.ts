13b5e7c890a7cd838450e4a7bb63bd4860e3f78e
Roger-iin <rogersimoes@hotmail.com>
2026-06-25 11:36:52 -0300

    feat: Cria o service para criar um usuario e configura a anotation @InjectRepository no users.module

---
+++ src/users/users.service.ts
@@ -0,0 +1,34 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>
+    ) {}
+
+    async create(createUserDto: CreateUserDto): Promise<User> {
+        const existingUser = await this.userRepository.findOne({
+            where: { email: createUserDto.email }
+        });
+
+        if (existingUser) {
+            throw new ConflictException("Este email já está em uso.")
+        }
+
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(createUserDto.password, saltRounds)
+
+        const user = this.userRepository.create({
+            ...createUserDto,
+            password: hashedPassword,
+        })
+
+        return this.userRepository.save(user)
+    }
+}

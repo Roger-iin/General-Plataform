979bfad94367789763a7d96ea63d8a3a9f978b2d
Roger-iin <rogersimoes@hotmail.com>
2026-06-25 11:53:01 -0300

    feat: Adiciona os validators ao create-user-dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,17 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+
 export class CreateUserDto {
+
+    @IsString({ message: 'O nome deve ser um texto.'})
+    @IsNotEmpty({ message: 'O nome deve ser informado.'})
     name!: string;
+
+    @IsEmail({}, { message: 'Forneça um email válido.'})
+    @IsNotEmpty({message: 'O email deve ser informado.'})
     email!: string;
+
+    @IsString({message: 'A senha deve ser um texto'})
+    @IsNotEmpty({message: 'A senha é obrigatória.'})
+    @MinLength(6, {message: 'A senha deve ter no mínimo 6 caracteres.'})
     password!: string;
 }

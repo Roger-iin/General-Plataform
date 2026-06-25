f4527e38dae60148bf6b4f47004da510cd379fa1
Roger-iin <rogersimoes@hotmail.com>
2026-06-25 14:42:19 -0300

    feat: Adiciona o finfByEmail no User.services

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -31,4 +31,10 @@ export class UsersService {
 
         return this.userRepository.save(user)
     }
+
+    async findByEmail(email: string): Promise<User | null>{
+        return this.userRepository.findOne({
+            where: {email},
+        })
+    }
 }

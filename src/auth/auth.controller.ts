a15ca97f6eefe8634a5151a127dea98eed4d8fe9
Roger-iin <rogersimoes@hotmail.com>
2026-06-26 13:21:49 -0300

    feat: Adiciona a rota para Login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 
